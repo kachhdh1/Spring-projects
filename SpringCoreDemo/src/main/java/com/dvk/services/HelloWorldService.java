51524a5cc50e8afe1020e28cf0d58e64c5213926
kachhdh1 <dharmik.kachhia@novartis.com>
2016-03-31 18:35:33 +0530

    Initial commit
    Hello world demo

---
+++ SpringCoreDemo/src/main/java/com/dvk/services/HelloWorldService.java
@@ -0,0 +1,19 @@
+package com.dvk.services;
+
+import org.springframework.stereotype.Service;
+
+@Service("helloWorldService")
+public class HelloWorldService {
+	
+	private String name;
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String sayHello() {
+		return "Hello! " + name;
+	}
+
+}
